ccfe54358e16eed92c439be42c7bc9d97e674b19
Douglas Henrique <douglas.henrique@strides.digital>
2021-08-03 14:29:19 -0300

    try menu 15

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -13,24 +13,28 @@ export function Navbar(props) {
   const isMobile = useMediaQuery({ maxWidth: DeviceSize.mobile });
 
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.leftSection}>
-            <Logo />
-          </div>
-          <div className={styles.middleSection}>
-            {!isMobile && <NavLinks />}
-          </div>
-          <div className={styles.rightSection}>
-            {!isMobile && <Accessibility /> || <MobileNavLinks />}
 
-
-          </div>
-
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.leftSection}>
+          <Logo />
         </div>
+        <div className={styles.middleSection}>
+          {!isMobile && <NavLinks />}
+        </div>
+        {!isMobile && (
+          <div className={styles.rightSection}>
+            <Accessibility />
+          </div>
+        )}
+
+        {isMobile && (
+          <div className={styles.rightSection}>
+            <MobileNavLinks />
+          </div>
+        )}
 
       </div>
-    </>
+    </div>
   );
 }
