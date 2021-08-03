cfa569c5f202693029403f1300c75dfe4c8e361f
Douglas Henrique <douglas.henrique@strides.digital>
2021-08-03 14:25:22 -0300

    menu try 14

--- src/components/navbar/mobileNavLinks.jsx
+++ src/components/navbar/mobileNavLinks.jsx
@@ -8,32 +8,30 @@ export function MobileNavLinks(props) {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <>
-      <div className={styles.navLinksContainer}>
-        <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
-        {isOpen && (
-          <ul>
-            <li>
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/pricing">
-                <a>Pricing</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-                <a>Blog</a>
-              </Link>
-            </li>
-            <div className={styles.marginer} />
-            <Accessibility />
-          </ul>
-        )}
+    <div className={styles.navLinksContainer}>
+      <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
+      {isOpen && (
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/pricing">
+              <a>Pricing</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a>Blog</a>
+            </Link>
+          </li>
+          <div className={styles.marginer} />
+          <Accessibility />
+        </ul>
+      )}
 
-      </div>
-    </>
+    </div>
   );
 }
