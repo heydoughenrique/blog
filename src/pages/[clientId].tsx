7a421922f6cc1989649b586a6f6e6e70ee3c3c67
Douglas Henrique <douglas.henrique@strides.digital>
2021-08-26 11:22:36 -0300

    testing clientid redirect

--- src/pages/[clientId].tsx
+++ src/pages/[clientId].tsx
@@ -1,9 +1,20 @@
 // AKA 404 Page
 import { useRouter } from 'next/router'
+import redirect from 'nextjs-redirect'
 
 const ClientDashboard = () => {
   const router = useRouter()
   const { clientId } = router.query
+
+  const Redirect = redirect('https://www.npmjs.com/package/nextjs-redirect')
+
+  if (clientId === '100') {
+    return (
+      <Redirect>
+        <h1>Redirecting to github!</h1>
+      </Redirect>
+    )
+  }
 
   return <p>You tried to access the page: https://strides.digital/{clientId} but it doesn't exist. Sorry</p>;
 }
