28352e0d6e797c2df204371e3cfc145222d09120
Douglas Henrique <douglas.henrique@strides.digital>
2021-08-30 22:23:41 -0300

    added new notion dash

--- src/pages/[clientId].tsx
+++ src/pages/[clientId].tsx
@@ -1,4 +1,4 @@
-// AKA 404 Page
+// AKA 404 Page 
 import { useRouter } from 'next/router'
 import redirect from 'nextjs-redirect'
 
@@ -11,10 +11,13 @@ const ClientDashboard = () => {
   if (clientId === '100') {
     return (
       <Redirect>
-        <h1>Redirecting to github!</h1>
+        <h1>Redirecting to Client Dashboard!</h1>
       </Redirect>
     )
   }
+
+
+
 
   return <p>You tried to access the page: https://strides.digital/{clientId} but it doesn't exist. Sorry</p>;
 }
